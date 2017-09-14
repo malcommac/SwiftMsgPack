2b3c8541aad15b7cdf9fd8de9659de90a9bdb4d7
Daniele Margutti <hello@danielemargutti.com>
2017-09-14 21:49:40 +0200

    Swift 4 Compatibility

--- Sources/SwiftMsgPack/Decoder.swift
+++ Sources/SwiftMsgPack/Decoder.swift
@@ -297,10 +297,10 @@ public extension Data {
 		// STRING 32 BIT LENGTH
 		// str 32	11011011	0xdb
 		case 0xdb:
-			let len_data =  Int(try stream.read8Bit()) << 24 +
-							Int(try stream.read8Bit()) << 16 +
-							Int(try stream.read8Bit()) << 8 +
-							Int(try stream.read8Bit())
+			var len_data = Int(try stream.read8Bit()) << 24
+			len_data += Int(try stream.read8Bit()) << 16
+			len_data += Int(try stream.read8Bit()) << 8
+			len_data += Int(try stream.read8Bit())
 			return try unpack(string: &stream, length: len_data)
 
 			
--- SwiftMsgPack.podspec
+++ SwiftMsgPack.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name = 'SwiftMsgPack'
-  spec.version = '0.9.1'
+  spec.version = '1.0.0'
   spec.summary = 'MsgPack Encoder/Decoder in pure Swift'
   spec.homepage = 'https://github.com/malcommac/SwiftMsgPack'
   spec.license = { :type => 'MIT', :file => 'LICENSE' }
